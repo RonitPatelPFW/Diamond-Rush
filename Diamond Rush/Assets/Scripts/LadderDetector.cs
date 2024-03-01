bbd6d38382d57811f77579d2dc851c3c7e42f3bc
Ronit Patel <ronitpatel884@gmail.com>
2024-02-29 22:00:40 -0500

    ladder movement

---
+++ Diamond Rush/Assets/Scripts/LadderDetector.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderDetector : MonoBehaviour
+{
+    [SerializeField] private PlayerMovement player;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Ladder>())
+        {
+            player.ClimbingAllowed = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Ladder>())
+        {
+            player.ClimbingAllowed = false;
+        }
+    }
+}
--- Diamond Rush/Assets/Scripts/PlayerMovement.cs
+++ Diamond Rush/Assets/Scripts/PlayerMovement.cs
@@ -2,45 +2,72 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+    private Rigidbody2D rb;
 
-	public CharacterController2D controller;
-	public Animator animator;
+    public bool ClimbingAllowed { get; set; }
+	float climbSpeed;
+    public float runSpeed = 40f;
 
-	public float runSpeed = 40f;
+    private float horizontalMove;
+    private bool jump = false;
 
-	float horizontalMove = 0f;
-	bool jump = false;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
-	// Update is called once per frame
-	void Update () {
+    private void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
-		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
 
-		if (Input.GetButtonDown("Jump"))
-		{
-			jump = true;
-			animator.SetBool("IsJumping", true);
-		}
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (ClimbingAllowed)
+            {
+                animator.SetBool("climbing", true);
+            }
+        }
+        else if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            animator.SetBool("climbing", false);
+        }
+    }
 
-	}
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
 
-	public void OnLanding ()
-	{
-		animator.SetBool("IsJumping", false);
-	}
+    private void FixedUpdate()
+    {
+        // Move the character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+        jump = false;
 
-	public void OnCrouching (bool isCrouching)
-	{
-		animator.SetBool("IsCrouching", isCrouching);
-	}
-
-	void FixedUpdate ()
-	{
-		// Move our character
-		controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
-		jump = false;
-	}
+        if (ClimbingAllowed)
+        {
+            climbSpeed = Input.GetAxisRaw("Vertical") * 5f;
+            rb.isKinematic = true;
+			rb.gravityScale = 0f;
+            rb.velocity = new Vector2(horizontalMove, climbSpeed);
+        }
+        else
+        {
+            rb.isKinematic = false;
+			rb.gravityScale = 4f;
+            rb.velocity = new Vector2(horizontalMove, rb.velocity.y);
+        }
+    }
 }
